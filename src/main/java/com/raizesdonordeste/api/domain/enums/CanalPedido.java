b049a14e6fc8edac6fd9c4eb42a450558fd359dc
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2026-02-26 16:59:57 -0300

    feat: adiciona enum CanalPedido requisito obrigatório

---
+++ src/main/java/com/raizesdonordeste/api/domain/enums/CanalPedido.java
@@ -0,0 +1,18 @@
+package com.raizesdonordeste.api.domain.enums;
+
+public enum CanalPedido {
+
+    APP(1, "Aplicativo"),
+    SITE(2, "Site"),
+    TOTEM(3, "Totem de Autoatendimento"),
+    PICKUP(4, "Retirada no Local"),
+    BALCAO(5, "Pedido no Balcão da Loja");
+
+    private final int cod;
+    private final String descricao;
+
+    private CanalPedido(int cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+}
