8f55219ffc0b928d5a1a130f277b26c80521c7d7
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2026-03-10 20:53:52 -0300

    feat(dto) : Implementa UsuarioRespostaDTO

---
+++ src/main/java/com/raizesdonordeste/api/dto/usuario/UsuarioRespostaDTO.java
@@ -0,0 +1,24 @@
+package com.raizesdonordeste.api.dto.usuario;
+
+import com.raizesdonordeste.domain.enums.PerfilUsuario;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioRespostaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private PerfilUsuario perfil;
+    private boolean ativo;
+    private boolean consentimentoProgramaFidelidade;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+}
+
