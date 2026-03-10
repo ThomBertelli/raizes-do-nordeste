a58e2cbbf129ab66894ab2561ccef3ac9cca7629
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2026-03-10 20:10:47 -0300

    feat(dto): cria classe LoginResponse

---
+++ src/main/java/com/raizesdonordeste/api/dto/auth/LoginResponse.java
@@ -0,0 +1,22 @@
+package com.raizesdonordeste.api.dto.auth;
+
+import com.raizesdonordeste.domain.enums.PerfilUsuario;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponse {
+
+    private String accessToken;
+    private Long expiresIn;
+    @Builder.Default
+    private final String tipo = "Bearer";
+    private Long id;
+    private String nome;
+    private String email;
+    private PerfilUsuario perfil;
+}
+
