5e4787617260ffa70a8e66996abb25ad36f065cf
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2026-03-19 17:44:15 -0300

    feat(config) : adiciona logs Slf4j ao JwtAutenticationFilter

--- src/main/java/com/raizesdonordeste/config/JwtAuthenticationFilter.java
+++ src/main/java/com/raizesdonordeste/config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,6 +19,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -44,21 +46,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } else if (StringUtils.hasText(token)) {
-                logger.warn(String.format(
-                        "JWT invalido para %s %s (remote: %s)",
-                        request.getMethod(),
-                        request.getRequestURI(),
-                        request.getRemoteAddr()
-                ));
+                log.warn("JWT invalido para {} {} (remote: {})", request.getMethod(), request.getRequestURI(), request.getRemoteAddr());
             }
         } catch (Exception e) {
-            logger.error(
-                    String.format(
-                            "Falha ao definir autenticacao via JWT para %s %s (remote: %s)",
-                            request.getMethod(),
-                            request.getRequestURI(),
-                            request.getRemoteAddr()
-                    ),
+            log.error(
+                    "Falha ao definir autenticacao via JWT para {} {} (remote: {})",
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    request.getRemoteAddr(),
                     e
             );
         }
